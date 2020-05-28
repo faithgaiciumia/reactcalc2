6530f50f9782cd45513c02b9970ae032621a92d4
faithgaiciumia <faithgaiciumia078@gmail.com>
2020-05-28 23:40:29 +0300

    updated css

---
+++ src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import DisplayEquation from './DisplayEquation';
+import DisplayResult from './DisplayResult';
+
+const display = () => (
+    <section className="screen">
+        <DisplayEquation/>
+        <DisplayResult/>
+
+    </section>
+)
